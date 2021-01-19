077d3239f17f0d4c825ba06e61229a2e08e7579d
181192 <k@kalli.no>
2021-01-19 13:10:45 +0100

    Ensure cluster is running

--- k8s-monitoring/01-prom-op-slo-tutorial/intro.sh
+++ k8s-monitoring/01-prom-op-slo-tutorial/intro.sh
@@ -6,9 +6,4 @@ mkdir -p /root/manifests/grafana
 mkdir -p /root/manifests/prometheus
 mkdir -p /root/manifests/prometheus-operator
 
-docker pull quay.io/prometheus-operator/prometheus-operator:v0.44.0
-docker pull quay.io/prometheus-operator/prometheus-config-reloader:v0.44.0
-docker pull quay.io/prometheus/prometheus:v2.22.1
-docker pull kiwigrid/k8s-sidecar:1.1.0
-docker pull grafana/grafana:7.3.5
-docker pull quay.io/prometheus/alertmanager:v0.21.0
+launch.sh
